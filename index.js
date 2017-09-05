7d9dc9cbb6f2aa6b1724849a492a8216c88aad13
zoe-c <find.zoec@gmail.com>
2017-09-05 15:52:28 -0400

    clean up and comment out for access to views in index.js

--- index.js
+++ index.js
@@ -37,184 +37,204 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // validation
 app.use(expressValidator());
 
-// ---------------------------------------------
-app.get('/', function(req,res){
+// LOGIN---------------------------------------------
+app.get('/', function(req,res) {
    res.render('login');
 });
 
-app.post('/login', function (req,res){
+app.post('/login', function(req,res) {
   let username = req.body.username;
   let password = req.body.password;
-
   models.gabber.findOne({
     where: {
       username: username,
       password: password
     }
-}).then(gabber => {
-    if (gabber.password == password && gabber.username == username) {
-      req.session.username = username;
-      req.session.gabberId = gabber.id;
-      req.session.authenticated = true;
-      res.redirect('/home');
+ }).then(gabber => {
+      if (gabber.password == password && gabber.username == username) {
+        req.session.username = username;
+        req.session.gabberId = gabber.id;
+        req.session.authenticated = true;
+        res.redirect('/home');
     } else {
-      req.session.authenticated = false;
-      console.log('unauthorized!');
-      res.redirect('/');
+        req.session.authenticated = false;
+        console.log('unauthorized!');
+        res.redirect('/');
     }
   })
   return req.session;
 });
 
-//------------------------------------------------------------------
-app.get('/signup', function (req,res) {
-   res.render('signup');
+// SIGNUP------------------------------------------------------------------
+app.get('/signup', function(req,res) {
+  res.render('signup');
 });
 
-app.post('/sign-up', function (req, res) {
-   const gabber = models.gabber.build({
-      username: req.body.username,
-      password: req.body.password1
-   });
-   gabber.save().then(function(req, res, next){
-      console.log(username);
-      next();
-   });
-  res.redirect('/');
+app.post('/sign-up', function(req, res) {
+  const gabber = models.gabber.build({
+    username: req.body.username,
+    password: req.body.password1
+  });
+  gabber.save().then( res.redirect('/') );
+ });
+
+// HOME-------------------------------------------------------------------------
+// NOTE: ***** un-comment after views are finished >>>>>>>>>>>>>>>>>>>>>>>>>>>
+// app.get('/home', function(req,res) {
+//   if (req.session & req.session.authenticated) {
+//     res.render('home', { username: req.session.username })
+//   } else {
+//     res.redirect ('/')
+//   }
+// });
+
+// NOTE: ***** delete after views are finished >>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get ('/home', function(req, res) {
+  res.render('home', { username: req.session.username });
 });
 
-// -------------------------------------------------------------
-app.get('/home', function (req,res) {
-   res.render('home', {username: req.session.username})
+
+// GOTTA-GAB (new post form)----------------------------------------------------
+// NOTE: ***** un-comment after views are finished >>>>>>>>>>>>>>>>>>>>>>>>>>>
+// app.get('/gotta-gab', function(req,res) {
+//   if (req.session & req.session.authenticated) {
+//       res.render('got-a-gab', { username: req.session.username });
+//     } else {
+//       res.redirect ('/');
+//     }
+// });
+// NOTE: ***** delete after views are finished >>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get('/gotta-gab', function(req,res) {
+   res.render('got-a-gab', { username: req.session.username });
 });
 
-app.get('/gotta-gab', function (req,res) {
-   res.render('got-a-gab', {username: req.session.username})
-});
-
-// -------------------------------------------------------------
-app.post('/post-gab', function (req, res) {
+app.post('/post-gab', function(req, res) {
    const post = models.post.build({
       title: req.body.gabTitle,
       body: req.body.gabBody,
       gabberId: req.session.gabberId
    })
-   post.save().then(function (newPost) {
-      console.log(newPost);
-      res.redirect('/community-gabs');
+   post.save().then(function(post) {
+      // console.log(post);
+      res.redirect('/gotta-gab');
    });
 });
-// ------------------------------------------------------------
-app.get('/community-gabs', function (req,res) {
+
+
+// COMMUNITY-GABS---------------------------------------------------------------
+// NOTE: ***** un-comment after views are finished >>>>>>>>>>>>>>>>>>>>>>>>>>>
+// app.get('/community-gabs', function(req,res) {
+//   if (req.session & req.session.authenticated) {
+//       models.post.findAll({
+//         order: [['id', 'DESC']],
+//         include: [{ model: models.gabber, as: 'gabber'}]
+//        }).then(function(posts) {
+//          res.render('gabble-community', { posts: posts });
+//        })
+//     } else {
+//       res.redirect ('/');
+//     }
+// });
+
+// NOTE: ***** delete after views are finished >>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get('/community-gabs', function(req,res) {
    models.post.findAll({
       order: [['id', 'DESC']],
-      include: [
-         {
-            model: models.gabber,
-            as: 'gabber'
-         }
-      ]
+      include: [{ model: models.gabber, as: 'gabber' }]
    }).then(function(posts) {
       res.render('gabble-community', { posts: posts })
    });
 });
 
-// like a post
-app.post('/like', function (req,res) {
+// like a post on community-gabs
+app.post('/like', function(req,res) {
    const like = models.like.build({
-         status: true,
-         postId: req.body.likeButton,
-         gabberId: req.session.gabberId
+     status: true,
+     postId: req.body.likeButton,
+     gabberId: req.session.gabberId
    });
-   // if they've already liked the post, do not save it. need to add function before the save below.
-   like.save().then(function (newLike) {
-      console.log(newLike);
-   });
-   // eventually, alert that they liked the post and then send them back to the gaggle.
-   res.redirect('/community-gabs');
+   like.save().then( res.redirect('/community-gabs') );
 });
 
-// -----------------------------------------------------
- app.get('/likes', function (req,res) {
+// LIKES------------------------------------------------------------------------
+// NOTE: ***** un-comment after views are finished >>>>>>>>>>>>>>>>>>>>>>>>>>>
+// app.get('/likes', function (req,res) {
+//   if (req.session & req.session.authenticated) {
+//     res.render('likes');
+//   } else {
+//     res.redirect('/');
+//   }
+// });
+
+// NOTE: ***** delete after views are finished >>>>>>>>>>>>>>>>>>>>>>>>>>>
+ app.get('/likes', function(req,res) {
    res.render('likes');
 });
 
-app.get('/likesGiven', function (req, res) {
+app.get('/likesGiven', function(req, res) {
   models.like.findAll({
     include: [
-       {
-          model: models.post,
-          as: 'post'
-       },
-       {
-          model: models.gabber,
-          as: 'gabber'
-       }
+       { model: models.post, as: 'post' },
+       { model: models.gabber, as: 'gabber' }
     ],
-    where: {
-       gabberId: req.session.gabberId,
-    }
+    where: { gabberId: req.session.gabberId }
  }).then(function(likes) {
-   res.render('likes', { likes: likes })
+      res.render('likes', { likes: likes })
    });
- });
+});
 
- app.get('/likesRecieved', function (req, res) {
+ app.get('/likesRecieved', function(req, res) {
    models.like.findAll({
      include: [
-        {
-           model: models.post,
-           as: 'post'
-        },
-        {
-           model: models.gabber,
-           as: 'gabber'
-        }
+        { model: models.post, as: 'post' },
+        { model: models.gabber, as: 'gabber' }
      ],
-     where: {
-        gabberId: {
-          $ne: req.session.gabberId,
-        }
-     }
+     where: { gabberId: { $ne: req.session.gabberId } }
   }).then(function(likes) {
-    res.render('likes', { likes: likes })
+      res.render('likes', { likes: likes })
     });
-  });
+});
 
-// -----------------------------------------------------------
-//delete post
-app.get('/your-gabs', function (req,res) {
+// YOUR-GABS--------------------------------------------------------------------
+// NOTE: ***** un-comment after views are finished >>>>>>>>>>>>>>>>>>>>>>>>>>>
+// app.get('/your-gabs', function(req,res) {
+//   if (req.session & req.session.authenticated) {
+//     models.post.findAll({
+//        where: { gabberId: req.session.gabberId }
+//     }).then(function(posts) {
+//        res.render('your-posts', { posts: posts })
+//     });
+//   } else {
+//     res.redirect('/');
+//   }
+// });
+
+// NOTE: ***** delete after views are finished >>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get('/your-gabs', function(req,res) {
    models.post.findAll({
-      where: {
-         gabberId: req.session.gabberId
-      }
+      where: { gabberId: req.session.gabberId }
    }).then(function(posts) {
-      res.render('your-posts', {posts: posts})
+      res.render('your-posts', { posts: posts })
    });
 });
 
 app.post('/delete', function(req,res) {
    models.like.destroy({
-     where: {
-       postId: req.body.deleteButton
-     }
-  }).then(
-    models.post.destroy({
-         where: {
-            id: req.body.deleteButton
-         }
-      })).then(
-         res.redirect('/your-gabs'));
+     where: { postId: req.body.deleteButton }
+  }).then(models.post.destroy({
+     where: { id: req.body.deleteButton }
+   })).then( res.redirect('/your-gabs') );
 
 });
-// logout
-// ---------------------------------------------------------
-app.post('/logout', function (req,res) {
+
+
+// LOGOUT---------------------------------------------------------
+app.post('/logout', function(req,res) {
    req.session.destroy();
    res.redirect('/gab-bye');
 });
 
-app.get('/gab-bye', function (req,res) {
+app.get('/gab-bye', function(req,res) {
    req.session.destroy();
    res.send('See ya later!');
 });
@@ -225,7 +245,4 @@ app.listen(process.env.PORT || 3000, function() {
 
 // app.listen(app.get('port'), function() {
 //   console.log('Node app is running on port', app.get('port'));
-// });
-// app.listen(3000, function() {
-//    console.log('Listening on port 3000!');
 // });
